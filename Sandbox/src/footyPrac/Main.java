99422323beb5d8e3c7a972f9fa668f873b55e6c5
inmiakoh <inmiakoh@localhost>
2025-10-09 21:26:05 +0100

    Applying methods

--- Sandbox/src/footyPrac/Main.java
+++ Sandbox/src/footyPrac/Main.java
@@ -1,22 +1,24 @@
 package footyPrac;
 
 public class Main {
+    public static void main(String[] args) {
+        Player p1 = new Player();
+        p1.playerName = "Salah";
+        p1.playerPosition = "RW";
+        p1.noOfGoals = 1;
+        p1.fitnessLevel = 70;   // start here
+        p1.isInjured = false;
 
-	public static void main(String[] args) {
-		
-		Player p1 = new Player();
-		p1.playerName = "Salah";
-		p1.fitnessLevel = 73;
-		p1.train();
-		p1.train();
-		
-		Player p2 = new Player();
-		p2.playerName = "Ekitike";
-		p2.fitnessLevel = 83;
-		p2.train();
-		p2.train();
-		
+        // Train twice
+        p1.train();  // 80, "did not train enough"
+        p1.train();  // 90, "was amazing"
 
-	}
+        // Play a match where he scores 2
+        p1.playMatch(2); // goals = 3, fitness = 70
 
+        // Play another match where he scores 1
+        p1.playMatch(1); // goals = 4, fitness = 50
+
+        System.out.println(p1.getStatus());
+    }
 }
--- Sandbox/src/footyPrac/Player.java
+++ Sandbox/src/footyPrac/Player.java
@@ -23,5 +23,37 @@ public class Player {
             System.out.println(playerName + " is already at peak fitness!");
         }
     }
+    
+    public void playMatch(int goalsScored) {
+    	//Play a match: add goals, reduce fitness, track the possibility of getting injured
+    	//This is at the top of the method because there is no point running the full code when the player is injured
+    	//The return there basically tells the program if the player is injured don't bother doing the rest of the method 
+    	//which is why its best practice to start with any liabilities
+    	//The stuff in the bracket is a "Parameter" which provides data to method 
+ 
+    	if (isInjured) {
+    		System.out.println(playerName + " cannot play today because they are injured");
+    		return;
+    	}
+    	
+    	//To update the number of goals each player scores
+    	noOfGoals = noOfGoals + goalsScored;
+    	
+    	//reduce fitness levels by 20 for the match
+    	fitnessLevel = fitnessLevel - 20;
+    	
+    	// injury rule: below 30 => injured
+        if (fitnessLevel < 30) {
+            isInjured = true;
+            System.out.println(playerName + " played and scored " + goalsScored + " goal(s), but got injured. Fitness: " + fitnessLevel);
+        } else {
+            System.out.println(playerName + " played and scored " + goalsScored + " goal(s). Fitness now: " + fitnessLevel);
+        }
+    }
 
-}
+    // Quick status string (return example)
+    public String getStatus() {
+        return playerName + " (" + playerPosition + ") - Goals: " + noOfGoals + ", Fitness: " + fitnessLevel + ", Injured: " + isInjured;
+    }
+    		
+   }
