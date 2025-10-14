af6e196a88f9e91eba94a0b26751be4a8b6e6418
inmiakoh <inmiakoh@localhost>
2025-10-14 21:14:32 +0100

    exploring returns

---
+++ Sandbox/src/tempConv/Converter.java
@@ -0,0 +1,10 @@
+package tempConv;
+
+public class Converter {
+	
+	public double celsiusToFahrenheit(double celcius) {
+		double fahrenheit  = (celcius * 9.0 / 5.0) + 32;
+		return fahrenheit;
+	}
+
+}
---
+++ Sandbox/src/tempConv/Main.java
@@ -0,0 +1,10 @@
+package tempConv;
+
+public class Main {
+    public static void main(String[] args) {
+        Converter c = new Converter();
+
+        System.out.println(c.celsiusToFahrenheit(0));   
+        System.out.println(c.celsiusToFahrenheit(1222)); 
+    }
+}
